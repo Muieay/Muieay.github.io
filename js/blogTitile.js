a28d7a399e2faff119bc3b9a6c72cf36fd1231c6
Muieay <muieay@qq.com>
2025-07-10 19:33:22 +0800

    Site updated: 2025-07-10 19:32:51

--- js/blogTitile.js
+++ js/blogTitile.js
@@ -1,1 +1,1 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="w(ﾟДﾟ)w 不要走！",clearTimeout(titleTime)):(document.title="♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="w(ﾟДﾟ)w 不要走！",clearTimeout(titleTime)):(document.title="♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
